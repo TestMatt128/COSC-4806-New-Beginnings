cb5e0f08c9ddc5d42cbee0b36f0fa216e64b3169
Matt <mdelfierro@algomau.ca>
2025-05-20 18:27:38 +0000

    Added some HTML code.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo '<p>Hello Class!</p>'; ?> 
+    <h1> Welcome to my PHP page </h1> 
+      
 
 </html>
